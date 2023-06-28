87c04eebc8c0f3662887e871e073b59f1e1d0edd
yenilikci <m.melihcelik017@outlook.com>
2023-06-28 18:52:43 +0300

    lesson4

---
+++ 4-global-styles-tailwind/src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import banner from "./assets/banner.png";
+import Card from './components/Card';
+
+function App() {
+
+  return (
+    <div class='container m-auto'>
+      <header>
+        <h1>Ninja Merch</h1>
+      </header>
+
+      <img class='rounded-md' src={banner} alt="site banner" />
+
+      <div class='grid grid-cols-4 gap-10 my-4'>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+    </div>
+  );
+
+}
+
+export default App;
---
+++ 4-global-styles-tailwind/src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (
+        <div class="bg-white p-4 text-center rounded-md shadow-sm">
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse totam, debitis voluptate dolorem non beatae ex eaque laboriosam autem corporis laudantium dolores iste temporibus officiis tempore odio quisquam ipsam eos?</p>
+            <button class="btn">Click me!</button>
+        </div>
+    )
+}
