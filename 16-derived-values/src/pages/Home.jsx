8121b9a83d6cc9cae797487f9725919ccf9212b3
yenilikci <m.melihcelik017@outlook.com>
2023-06-29 22:02:45 +0300

    lesson 16

---
+++ 16-derived-values/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { Show, createResource } from "solid-js";
+import Card from "../components/Card";
+import { Route, Routes, A } from "@solidjs/router";
+
+const fetchProducts = async () => {
+    const res = await fetch('http://localhost:4000/products')
+    return res.json()
+}
+
+export default function Home() {
+
+    const [products] = createResource(fetchProducts);
+
+    return (
+        <Show when={products()} fallback={<p>loading...</p>}>
+            <div class='grid grid-cols-4 gap-10 my-4'>
+                <For each={products()}>
+                    {(product) => (
+                        <Card rounded={true} flat={true}>
+                            <img src={product.img} alt="product img" />
+                            <h1 class="my-3 font-bold">{product.title}</h1>
+                            <A href={"/product/" + product.id} class="btn">
+                                View Product
+                            </A>
+                        </Card>
+                    )}
+                </For>
+
+            </div>
+        </Show>
+    )
+}
